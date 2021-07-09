12f01a44ccf9a60d8beeeddc8275f52ddefbccac
painttist <painttist@gmail.com>
2021-07-10 02:24:49 +0800

    Fixed Portal Bullet Warnings

--- IndieTale/Scenes/Portals/PortalBullet.gd
+++ IndieTale/Scenes/Portals/PortalBullet.gd
@@ -12,9 +12,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _integrate_forces(state):
-	var contact = state.get_contact_local_position(0)
-	if (contact != Vector2.ZERO):
-		print(contact)
-		get_node(portal_path).change_position(contact)
-#		print(get_viewport_rect().size.x)
-		queue_free()
+	if (state.get_contact_count() > 0) :
+		var contact = state.get_contact_local_position(0)
+		if (contact != Vector2.ZERO):
+			get_node(portal_path).change_position(contact)
+	#		print(get_viewport_rect().size.x)
+			queue_free()
