eb88984923e79ca38a0eedcb7877dcd043a6ac76
Alan Lai <lwl1135935755@gmail.com>
2021-07-10 00:15:41 +0800

    Basic Turret Builder and Turrets

---
+++ IndieTale/Scenes/Turrets/Turrets.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+enum {
+	Test,
+	White
+}
+
+var turret_type_names = ["res://Assets/Turrets/TurretTest.png", "res://Assets/Turrets/Turret.png"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	build_turret(Vector2(300, 300), deg2rad(50), White)
+
+
+# Return true if building success, return false if it fails
+# rot should be radius
+func build_turret(pos: Vector2, rot: float, turret_type) -> void:
+	print(pos, rot)
+	var turret_scene = load("res://Scenes/Turrets/Turret.tscn")
+	var turret_instance = turret_scene.instance()
+	self.add_child(turret_instance)
+	turret_instance.get_node("TurretSprite").texture = load(turret_type_names[turret_type])
+	turret_instance.position = pos
+	turret_instance.rotation = rot
+
---
+++ IndieTale/Scenes/UI/TurretBuilder.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+
+enum {
+	Nothing,
+	Blue,
+	Red,
+	Yellow,
+}
+
+var state = Nothing
+var building_phase = 0
+var turret_instance = null
+
+const turret_node_path = "res://Scenes/Turrets/Turret.tscn"
+const texture_path = ["res://Assets/Turrets/TurretTest.png", "res://Assets/Turrets/Turret.png"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(_delta):		
+	if state != Nothing:
+		building_turret()
+
+func get_mouse_rotation() -> float:
+	var normalized_vector = (get_global_mouse_position() - turret_instance.position).normalized()
+#	print(normalized_vector.cross(Vector2.UP))
+	if normalized_vector.y < 0:
+		return asin(Vector2.UP.cross(normalized_vector))
+	else:
+		return PI - asin(Vector2.UP.cross(normalized_vector))
+
+func building_turret() -> void:
+	# The first phase: position
+	if building_phase == 0:
+		turret_instance.position = get_global_mouse_position()
+		if Input.is_action_just_pressed("ui_left_click"):
+#			print("left click")
+			building_phase += 1
+
+	# The second phase: rotation
+	elif building_phase == 1:
+		turret_instance.rotation = get_mouse_rotation()
+		if Input.is_action_just_pressed("ui_left_click"):
+#			print("left click", turret_instance.rotation)
+			building_phase += 1
+
+	# The third phase: building
+	elif building_phase == 2:
+		pass
+
+func instance_turret() -> void:
+	turret_instance = load(turret_node_path).instance()
+	turret_instance.get_node("TurretSprite").texture = load(texture_path[state])
+	self.add_child(turret_instance)
+
+func _on_TurretBlue_pressed():
+	state = Blue
+	instance_turret()
