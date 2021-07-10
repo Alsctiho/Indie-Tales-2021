99b58caae9bb47dc052eb84d6cc68a326763b468
painttist <painttist@gmail.com>
2021-07-10 16:25:04 +0800

    Updated Monster Portal

--- IndieTale/Scenes/Player/Player.gd
+++ IndieTale/Scenes/Player/Player.gd
@@ -33,7 +33,7 @@ var is_portal_1 = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_node("Hitbox").connect("body_entered", self, "_on_Hitbox_body_entered")
 
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
